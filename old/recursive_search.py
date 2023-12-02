2e0663ee02adb5f6c42e50353304a89e99e8cb25
fabienmoreno <fabien.m@hiflow.com>
2023-12-02 22:13:35 +0100

    Successfull recursive search

--- old/recursive_search.py
+++ old/recursive_search.py
@@ -2,12 +2,14 @@ import time
 start=time.perf_counter()
 
 
-S=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+S=["one", "two", "se", "three", "four", "five", "six", "seven", "eight", "nine", "5"]
 graph_dict={}
 
 def build_dict(sub_dict, word):
-    if len(word)==2:
-        sub_dict[word[0]]=word[1]
+    if len(word)==1:
+        if word in sub_dict.keys():
+            sub_dict[word][None]=None
+        else: sub_dict[word]={None:None}
     else:
         if word[0] not in sub_dict.keys(): sub_dict[word[0]]={}
         sub_dict[word[0]]=build_dict(sub_dict[word[0]],word[1:])
@@ -16,21 +18,27 @@ def build_dict(sub_dict, word):
 for w in S:
     graph_dict=build_dict(graph_dict,w)
 
+print(graph_dict)
+
 print("Duration : ", (time.perf_counter()-start)*1000, " ms")
 
-R="khgdlljfxjt6sehvenfour35pxohne"
+R="khgdsfurellj5sefxjtsevenfour35pxohne"
 
 def search_graph(sub_dict, text):
+    global C
     if len(text)==0: return False
-    if type(sub_dict)==str:
-        if text[0]==sub_dict: return True
-        else: return False
-    elif type(sub_dict)==dict:
+    elif None in sub_dict.keys():
+        if sub_dict[None]==None: return True
+    else:
         if text[0] in sub_dict.keys() and search_graph(sub_dict[text[0]],text[1:]):
+            print(text[0])
+            C+=text[0] 
             return True
         else: return False
 
 def xsearch(sub_dict, text):
+    global C
+    C=""
     check=False
     for i in range(len(R)):
         if search_graph(graph_dict,R[i:]):
@@ -38,4 +46,32 @@ def xsearch(sub_dict, text):
             break
     return (check,i)
 
-print(xsearch(graph_dict, R))
+print(xsearch(graph_dict, R),C[::-1])
+
+exit(0)
+
+{
+    'o': {'n': {'e': {None: None}}}, 
+    't': {'w': {'o': {None: None}}, 'h': {'r': {'e': {'e': {None: None}}}}}, 
+    '3': {None: None}, 
+    'f': {'o': {'u': {'r': {None: None}}}, 'i': {'v': {'e': {None: None}}}}, 
+    's': {'i': {'x': {None: None}}, 'e': {'v': {'e': {'n': {None: None}}}, None: None}}, 
+    'e': {'i': {'g': {'h': {'t': {None: None}}}}}, 
+    'n': {'i': {'n': {'e': {None: None}}}}
+    }
+def search_graph_old(sub_dict, text):
+    global C
+    if len(text)==0: return False
+    if type(sub_dict)==str:
+        if text[0]==sub_dict:
+            print(text[0]) 
+            C+=text[0]
+            return True
+        else: return False
+    elif type(sub_dict)==dict:
+        if text[0] in sub_dict.keys() and search_graph(sub_dict[text[0]],text[1:]):
+            print(text[0])
+            C+=text[0] 
+            return True
+        else: return False
+
