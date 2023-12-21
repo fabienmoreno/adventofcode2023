a4f6d941f63549a47af1110f9945306a1d6f142b
fabienmoreno <fabien.m@hiflow.com>
2023-12-21 22:48:15 +0100

    clean part 1

--- puzzles/p04.py
+++ puzzles/p04.py
@@ -8,17 +8,11 @@ def part1(filename):
             D=C.draw_list()
             W=C.win_list()
             count=0
-            L=[]
             for i in D:
                 if i in W: 
                     count+=1
-                    L.append(i)
             if count==0: total=0
             else: total=2**(count-1)
-            #print(C.number(), total, L)
             result=result+total
     return result
 
-"""
-Card 211: 26 70 22 97 55 51 41 29 61 78 | 
-          12 15 84 16  3 38  2 43 66 24 10 71 48 45 53  1 13 23 69 35 74 40  5 77 52"""
